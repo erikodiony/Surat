82ae96f94bee1a7506594b90643890d9ff40808f
Erikodiony Ariessa Wahyudi <erikodiony@live.com>
2018-06-13 21:35:43 +0700

    Initial Release

--- Login/lupaKataSandi.php
+++ Login/lupaKataSandi.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>&nbsp ./Puskesmas &nbsp- &nbspPegawai &nbsp(Lupa Kata Sandi) &nbsp|</title>
+  <title>&nbsp ./Aplikasi&nbspSurat Dinsosnaker &nbsp- &nbspLupa Kata Sandi &nbsp|</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
